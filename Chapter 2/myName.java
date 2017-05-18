157c26170a7ea1145ddaab30a051019c79100ee9
pbfryman <blakefryman12@gmail.com>
2017-05-18 11:30:30 -0500

    Update myName.java

--- Chapter 2/myName.java
+++ Chapter 2/myName.java
@@ -2,8 +2,8 @@ public class myName
 {
    public static void main(String[] args)
    {
-      String name = "Blake";
-      int age = 23;
+      String name = "John Jacobs";
+      int age = 28;
       double annualPay = 100000;
       
       System.out.println("My name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.");
