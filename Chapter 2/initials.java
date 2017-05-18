bcb2971ca518d0c53e41ba0c4bf9d991797512f5
pbfryman <blakefryman12@gmail.com>
2017-05-18 11:30:10 -0500

    Update initials.java

--- Chapter 2/initials.java
+++ Chapter 2/initials.java
@@ -2,12 +2,12 @@ public class initials
 {
    public static void main(String[] args)
    {
-      String firstName = "Paul";
-      String middleName = "Blake";
-      String lastName = "Fryman";
-      char firstInitial = 'P';
+      String firstName = "John";
+      String middleName = "Bill";
+      String lastName = "Jacobs";
+      char firstInitial = 'J';
       char middleInitial = 'B';
-      char lastInitial = 'F';
+      char lastInitial = 'J';
       
       System.out.println("My name is " + firstName + " " + middleName + " " + lastName + ".");
       System.out.println("My initials are " + firstInitial + "." + middleInitial + "." + lastInitial + ".");
