1132c7a50f943800576350f046d6cb08d5a02f2a
pbfryman <blakefryman12@gmail.com>
2017-05-18 11:29:46 -0500

    Update personalInfo.java

--- Chapter 2/personalInfo.java
+++ Chapter 2/personalInfo.java
@@ -2,9 +2,9 @@ public class personalInfo
 {
    public static void main(String[] args)
    {
-      String name = "Paul Blake Fryman";
-      String address = "7055 Town Bluff Drive, Dallas, TX 75248";
-      String phone = "972-974-6999";
+      String name = "John Jacobs";
+      String address = "705 Street Name Drive, City, ST 75987";
+      String phone = "111-111-1111";
       String major = "Business Computer Information Systems";
       
       System.out.println(name + "\n" + address + "\n" + phone + "\n" + major);
